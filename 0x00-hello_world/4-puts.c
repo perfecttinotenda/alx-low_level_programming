fee380a2762359347fc5c6654dcc8b8371d7d1c2
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-02-16 07:15:49 -0800

    mos

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - A programm that prints a line with a function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
