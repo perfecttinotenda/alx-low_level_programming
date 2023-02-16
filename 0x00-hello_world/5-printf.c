dea450241b09900505df249ee9f2fa94ed8b0a51
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-02-16 07:30:39 -0800

    mos

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program thata prints a line using a printf without using puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
