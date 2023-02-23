2b10d0db27199032454e4a6b31d348ac2c485280
perfecttinotenda <perfecttinotendamashingaidze@gmail com>
2023-02-23 18:37:27 +0000

    Debugging 2

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - checks if given numbev
+ *
+ * @i: number to be checked 
+ * Return: nothing on Sucess
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
+}
