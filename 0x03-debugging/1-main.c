e679124a4ef57df2bbc68ad2e72136e05188ff30
perfecttinotenda <perfecttinotendamashingaidze@gmail com>
2023-02-23 19:34:20 +0000

    Debugging 3

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 	int i;
+
 	printf("Infinite loop incoming :(\n");
 	i = 0;
 
@@ -17,7 +18,6 @@ int main(void)
 	*	putchar(i);
 	*}
 	*/
-
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
