a6f4a197ae8bb79a90dbbfa2f9ad7b81c935c9c2
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-02-28 08:01:15 -0800

    q0

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - check the code 
+ * main - checking my code
  *
- * Return: Always 0.
+ * Return: 0 (success)
  */
 int main(void)
 {
-    int n;
+	int n;
 
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+	n = 402;
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	return (0);
 }
