7e6099632592bcc57e096dde354bbd440771779b
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-03-13 14:26:01 +0000

    Ah

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -4,6 +4,14 @@
 #include <ctype.h>
 #include <string.h>
 
+/**
+* _isupper - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+
 int _isupper(int c)
 {
 	return (c >= 'A' && c <= 'Z');
--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -4,7 +4,15 @@
 #include <ctype.h>
 #include <string.h>
 
+/**
+* _isdigit - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+
 int _isdigit(int c)
 {
-	return isdigit(c);
+	return (c >= '0' && c <= '9');
 }
