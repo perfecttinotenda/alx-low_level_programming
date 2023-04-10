6a7bb81a9cba439c088e4887aa2294d063612fa4
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-04-10 19:24:32 +0000

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,8 +10,7 @@
  * Return: the value of a bit at a given index.
  */
 
-int get_bit(unsigned long int n, unsigned int index)
-
+int get_bit(unsigned long int num, unsigned int index)
 {
 	int bit_num;
 
