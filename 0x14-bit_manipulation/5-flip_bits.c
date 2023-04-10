070944fbeed1931f2da8454b9399ad7cbfe45b85
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-04-10 19:30:06 +0000

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,19 +4,19 @@
 /**
  * flip_bits - function that returns the number of bits you would need to flip to get from one number to another.
  * to get from one number to another
- * @num: 1st number
+ * @k: 1st number
  * @may: 2sec number
  *
  * Return: number of bits to change
  */
 
-unsigned int flip_bits(unsigned long int num, unsigned long int may)
+unsigned int flip_bits(unsigned long int k, unsigned long int may)
 {
 	int num, read = 0;
 
-        unsigned long int now;
+	unsigned long int now;
 
-        unsigned long int exclusive = num ^ may;
+	unsigned long int exclusive = k ^ may;
 
 	for (num = 63; num >= 0; num--)
 	{
