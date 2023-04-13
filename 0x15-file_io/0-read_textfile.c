c1e4c9b6d43b02a7a1fc88a723f866a92b2d15b4
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-04-13 15:48:10 +0200

    task O again

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,20 +12,25 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	ssize_t fd = 0, w = 0, t = 0;
+	char *buf = NULL;
+
+	if (!filename)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+
 	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+
 	t = read(fd, buf, letters);
 	w = write(STDOUT_FILENO, buf, t);
 
 	free(buf);
 	close(fd);
+
 	return (w);
 }
-
